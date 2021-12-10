e9a01dd8f700241d36fc7d7a9c5751a3f3a4edf1
Ruth Wells <@citizensadvice.org.uk>
2021-12-10 16:27:41 +0000

    tidy up day_3.rb

--- day_3/day_3.rb
+++ day_3/day_3.rb
@@ -1,31 +1,19 @@
 # frozen_string_literal: true
-# get array
 data_array = File.readlines('data_file.txt')
 
-len = data_array.length
-# initialize our count
 count_checks = 0
 digits = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-data_array.each { |entry|
-  puts entry
+data_array.each do |entry|
   count_checks += 1
-  for i in 0..11
-    puts entry[i]
-    puts digits[i]
-    puts "_____"
-    digits[i] += entry[i].to_i
-  end
-
-}
-puts digits, count_checks
+  (0..11).each { |i| digits[i] += entry[i].to_i }
+end
 
 gamma = 0
 epsilon = 0
 
-
-for i in 0..11
+(0..11).each do |i|
   digits[i] > 500 ? gamma += 2**(11-i) : epsilon += 2**(11-i)
 end
 
-puts gamma, epsilon, gamma * epsilon
+puts gamma * epsilon
